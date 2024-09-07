e5c4267c8774b941e62c590ccb6b43f783a32751
HuyDũng <contact.huydung@gmail.com>
2024-09-07 12:05:23 +0700

    Modal with Links. Contact Form. Game Description.

---
+++ _posts/2024-09-02-prod-mini-game.markdown
@@ -0,0 +1,23 @@
+---
+layout: default
+modal-id: "zombie-escape"
+date: 2024-09-02
+img: zombie_escape.png
+alt: "A team of 3 people running away from zombie"
+project-date: September 2024
+project-name: "Zombie Escape: Puzzle Path"
+play-store-link: "https://play.google.com/store/apps/details?id=fun.cutezombies.ProdZombieEscape"
+---
+
+**Deadline is real, this time**. The terrifying, over-powered zombies are approaching! Save your team! 
+
+**Are your thinking skill sharp enough**? You need to be smart AND fast. Assign the right tasks to the right team members at the right time, and get to the escaping portals.
+
+- 100% fun, 100% free, no ads! Easy to pick up game with:
+- 20 levels of thrilling fun
+- 12 achievements to unlock
+
+*For people in Hanoi*: Unlock an extra special in-real-life reward when you unlock ALL the achievements
+
+This fun, tiny-but-mighty game is inspired by the job of a Video Game Producer / Project Manager.
+
--- js/freelancer.js
+++ js/freelancer.js
@@ -35,3 +35,33 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+
+  $(document).ready(function() {
+    console.log("Attaching modal event listeners");
+
+    // Handle modal show event to update the URL with the modal's ID
+    $('.modal').on('show.bs.modal', function (event) {
+
+      var modalId = $(this).attr('id');
+      var newUrl = window.location.pathname + '#' + modalId;
+      history.pushState(null, '', newUrl);
+      //console.log("Modal shown: " + modalId);
+    });
+  
+    // Handle modal hide event to remove the modal's ID from the URL
+    $('.modal').on('hidden.bs.modal', function (event) {
+      history.pushState(null, '', window.location.pathname);
+      //console.log("Modal hidden");
+    });
+  
+    // Check if there's a hash in the URL when the page loads, and show the relevant modal
+    var hash = window.location.hash;
+    if (hash) {
+      $(hash).modal('show');
+    }
+  });
+  
+  
+
+
